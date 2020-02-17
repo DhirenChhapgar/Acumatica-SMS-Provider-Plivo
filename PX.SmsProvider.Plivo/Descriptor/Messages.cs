24f13e9b4b63801e489cd1b5c3823c2af6cc1a10
dchhapgar <dchhapgar@acumatica.com>
2020-02-17 18:17:17 -0500

    * Plivo SMS Provider

---
+++ PX.SmsProvider.Plivo/Descriptor/Messages.cs
@@ -0,0 +1,10 @@
+﻿using PX.Common;
+
+namespace PX.SmsProvider.Plivo
+{
+    [PXLocalizable]
+    public static class Messages
+    {
+        public const string ProviderName = "Plivo SMS provider";
+    }
+}
---
+++ PX.SmsProvider.Plivo/PlivoSmsProviderFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace PX.SmsProvider.Plivo
+{
+    public class PlivoSmsProviderFactory : ISmsProviderFactory
+    {
+        //Create Provider and initialize with settings
+        public ISmsProvider Create(IEnumerable<ISmsProviderSetting> settings)
+        {
+            var provider = new PlivoSmsProvider();
+            provider.LoadSettings(settings);
+            return provider;
+        }
+
+        public ISmsProvider Create()
+        {
+            var provider = new PlivoSmsProvider();
+            return provider;
+        }
+
+        //Description of the Provider - the way you need it to be displayed in Provider Type box on the SMS Provider (SM203535) screen
+        public string Description { get; } = Messages.ProviderName;
+        //Name of the Provider - the way you need it to be displayed in Provider Type box on the SMS Provider (SM203535) screen
+        public string Name { get; } = typeof(PlivoSmsProvider).FullName;
+    }
+}
